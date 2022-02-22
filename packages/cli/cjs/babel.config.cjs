9cd290b3afb662a16a25e5ef2872400963145af5
3lang <675483520@qq.com>
2022-02-22 17:53:17 +0800

    chore: rm unuseless deps

--- packages/cli/cjs/babel.config.cjs
+++ packages/cli/cjs/babel.config.cjs
@@ -1,12 +1,7 @@
-// const babelJest = require('babel-jest');
-
-// module.exports = babelJest.createTransformer(babelOptions);
-
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
-  "env": {
-    "test": {
-      plugins: ['@babel/plugin-transform-runtime']
-    }
-  }
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+    '@babel/preset-react'
+  ],
 };
