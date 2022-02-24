1f200f277a3572eec05c09bb157258610a13fa2d
3lang <675483520@qq.com>
2022-02-24 14:53:57 +0800

    chore(cli): adjust site dir

--- packages/cli/site/src/main.jsx
+++ packages/cli/site/src/main.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SiteTheme from 'mdoc-theme-default';
+import { MdocSiteContext } from '@mdoc/theme';
+import * as shared from 'site-shared-lazy';
+
+const App = () => {
+  return (
+    <MdocSiteContext.Provider value={{ ...shared }}>
+      <SiteTheme />
+    </MdocSiteContext.Provider>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
