11b94a056895d3e029c096b66f1d8c57b545a064
3lang <675483520@qq.com>
2022-03-10 17:16:10 +0800

    docs(cli): rollup-postcss-plugin problem

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -6,6 +6,12 @@ import { build as viteBuild } from 'vite'
 import { PROJECT_DIST_DIR, PROJECT_ES_DIR, PROJECT_CJS_DIR } from '../common/constant';
 import { getViteConfigForPackage } from '../config/vite.package';
 
+// Vitejs build bundles
+// Postcss can't output every component style file.
+// fallback1: https://github.com/cutterbl/vitejs-plugin-inject-css/blob/master/src/index.js
+// fallback2: https://github.com/wxsms/vite-plugin-libcss/blob/master/index.js
+// fallback3: glup workflow https://juejin.cn/post/6962088402174869517#heading-4
+
 const compileBundles = async () => {
   const configs = [
     getViteConfigForPackage({ outputDir: PROJECT_ES_DIR, format: 'es' }),
