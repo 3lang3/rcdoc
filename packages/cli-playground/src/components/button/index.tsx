3b0ffcf22f30b5c98d29e65b6381da47a260bcd8
3lang <675483520@qq.com>
2022-03-10 17:12:48 +0800

    chore(playground): test umd build

--- packages/cli-playground/src/components/button/index.tsx
+++ packages/cli-playground/src/components/button/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Space } from 'mdoc-demo';
 
+import './index.less'
+
 export default ({ children, ...props }) => (
   <Space className="button" {...props}>
     {children}
