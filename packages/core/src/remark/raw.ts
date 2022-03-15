7e4f583629fe85ccfec6543bcf8f34e18e36e734
3lang <675483520@qq.com>
2022-03-15 11:27:04 +0800

    feat(core): support custom component tag

--- packages/core/src/remark/raw.ts
+++ packages/core/src/remark/raw.ts
@@ -21,6 +21,16 @@ export default function raw(): MDocUnifiedTransformer<MDocElmNode> {
     const props = [];
 
     visit(tree, node => {
+      // workaround to avoid lowercase for React Component tag name
+      // mark React Compnoent via mdoc-raw prefixer, eg API => mdoc-raw-api
+      if (typeof node.tagName === 'string' && /^[A-Z]/.test(node.tagName)) {
+        node.tagName = `mdoc-raw${node.tagName.replace(/[A-Z]/g, s => `-${s.toLowerCase()}`)}`;
+      } else if (typeof node.value === 'string' && node.type === 'raw') {
+        node.value = node.value.replace(/(<\/?)([A-Z]\w+)/g, (_, prefix, tagName) => {
+          return `${prefix}mdoc-raw${tagName.replace(/[A-Z]/g, s => `-${s.toLowerCase()}`)}`;
+        });
+      }
+
       // workaround to avoid parse React Component properties to string in the raw step
       if (typeof node.properties === 'object' && hasComplexProp(node.properties)) {
         props.push(node.properties);
@@ -34,6 +44,11 @@ export default function raw(): MDocUnifiedTransformer<MDocElmNode> {
       if (typeof node.value === 'string' && /<code[^>]*src=/.test(node.value)) {
         node.value = node.value.replace(/ ?\/?>/g, '></code>');
       }
+
+      // special process <API /> for same reason in above
+      if (typeof node.value === 'string' && /<mdoc-raw-a-p-i/.test(node.value)) {
+        node.value = node.value.replace(/ ?\/?>/g, '></mdoc-raw-a-p-i>');
+      }
     });
 
     // raw to hast tree
@@ -41,13 +56,15 @@ export default function raw(): MDocUnifiedTransformer<MDocElmNode> {
 
     // restore React Component & it's properties
     visit<MDocElmNode, string>(parsed, 'element', elm => {
+      // restore tag name
+      if (/^mdoc-raw/.test(elm.tagName)) {
+        elm.tagName = elm.tagName
+          .replace('mdoc-raw', '')
+          .replace(/-([a-z])/g, (_, word) => word.toUpperCase());
+      }
       // restore properties from temp array
       if (has(elm, '_index')) {
         elm.properties = props[elm.properties._index];
-      }
-
-      if (/^previewer/.test(elm.tagName)) {
-        elm.tagName = 'Previewer'
       }
     });
 
