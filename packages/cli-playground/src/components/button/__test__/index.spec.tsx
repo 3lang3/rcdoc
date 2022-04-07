a3d341057d0dbcb2abf9b0d812f3e9c1bd5805d9
3lang <675483520@qq.com>
2022-04-07 13:58:23 +0800

    chore(cli-playground): add button test case

--- packages/cli-playground/src/components/button/__test__/index.spec.tsx
+++ packages/cli-playground/src/components/button/__test__/index.spec.tsx
@@ -1,51 +1,15 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import Button from '../index';
+import TestRenderer from 'react-test-renderer';
+import Button from '..';
 
-import mountTest from '../../../tests/shared/mountTest';
+it('base render correctly', () => {
+  const baseRenderer = TestRenderer.create(<Button>base button</Button>);
+  const testInstance = baseRenderer.root;
 
-describe('Button', () => {
-  mountTest(Button);
-  mountTest(() => <Button type="primary" />);
+  const tree = baseRenderer.toJSON();
+  expect(tree).toMatchSnapshot();
 
-  it('renders correctly', () => {
-    expect(<Button />).toMatchRenderedSnapshot();
-  });
-
-  it('mount correctly', () => {
-    expect(() => mount(<Button />)).not.toThrow();
-  });
-
-  it('should render empty button without errors', () => {
-    const wrapper = mount(
-      <Button>
-        {null}
-        {undefined}
-      </Button>,
-    );
-    expect(wrapper.render()).toMatchSnapshot();
-  });
-
-  it('should simulate click event', () => {
-    const wrapper = mount(<Button />);
-    expect(wrapper.simulate('click').length).toEqual(1);
-  });
-
-  it('should not emit click event when disabled', () => {
-    const onClick = jest.fn();
-    const wrapper = mount(<Button disabled onClick={onClick} />);
-
-    wrapper.simulate('click');
-    expect(onClick).not.toHaveBeenCalled();
-  });
-
-  it('should hide border when color is gradient', () => {
-    const wrapper = mount(<Button color="linear-gradient(#000, #fff)" />);
-    expect(wrapper.getDOMNode().style.border).toEqual('0px');
-  });
-
-  it('should change icon class prefix when using icon-prefix prop', () => {
-    const wrapper = mount(<Button icon="success" />);
-    expect(wrapper.html()).toMatchSnapshot();
-  });
+  expect(
+    testInstance.findByProps({ className: 'rv-button__text' }).children,
+  ).toEqual(['base button']);
 });
--- packages/cli-playground/src/components/grid/__test__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Button from '..';
-
-it('Button default render', () => {
-  const component = renderer.create(<Button>Facebook</Button>);
-  let tree = component.toJSON() as any;
-  expect(tree).toMatchSnapshot();
-});
