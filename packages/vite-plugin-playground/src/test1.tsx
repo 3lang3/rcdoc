8b6d0e0434bf259c861ae759c67f366189b2710e
3lang <675483520@qq.com>
2022-04-13 15:15:05 +0800

    chore: rm vitejs plugin playground

--- packages/vite-plugin-playground/src/test1.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default () => {
-  const [count, setCount] = React.useState<number>(0)
-  return (
-    <button onClick={() => setCount(v => v + 1)}>test1.tsx: {count}</button>
-  )
-}
