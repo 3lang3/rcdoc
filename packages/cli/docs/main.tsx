da861efffa6a62a5dcc7ff42fceb5a484d82f58e
3lang <675483520@qq.com>
2022-05-24 17:39:04 +0800

    chore: update react 18

--- packages/cli/docs/main.tsx
+++ packages/cli/docs/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import SiteTheme from 'rcdoc-theme-default';
 import { MdocSiteContext } from '@rcdoc/theme';
 import * as shared from '@@rcdoc/site-shared';
@@ -127,9 +127,7 @@ const App = () => {
   );
 };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
