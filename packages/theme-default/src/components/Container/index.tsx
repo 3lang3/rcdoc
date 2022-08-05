f24689eca43b071a99e9645352b595853d9d635e
3lang <675483520@qq.com>
2022-08-05 09:13:24 +0800

    style: update default theme ui

--- packages/theme-default/src/components/Container/index.tsx
+++ packages/theme-default/src/components/Container/index.tsx
@@ -10,18 +10,23 @@ import './index.less';
 export default (props) => {
   const { config } = props;
   return (
-    <div
-      className={clsx('doc-container__wrarpper', {
-        'doc-container--menu': props.hasMenu,
-      })}
-    >
-      <Flex align="flex-start" className={clsx('doc-container', `page--${props.currentPageName}`)}>
-        <LinerLoader />
-        <div className="doc-container-markdown">{props.children}</div>
-        {!!config?.site?.themeConfig?.simulator && <Simulator />}
-        {config?.site?.slug === 'content' && <Slugs />}
-      </Flex>
+    <>
+      <div
+        className={clsx('doc-container__wrarpper', {
+          'doc-container--menu': props.hasMenu,
+        })}
+      >
+        <Flex
+          align="flex-start"
+          className={clsx('doc-container', `page--${props.currentPageName}`)}
+        >
+          <LinerLoader />
+          <div className="doc-container-markdown">{props.children}</div>
+          {!!config?.site?.themeConfig?.simulator && <Simulator />}
+          {config?.site?.slug === 'content' && <Slugs />}
+        </Flex>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
